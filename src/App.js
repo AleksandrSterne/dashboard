0da16bce8f1814937781d319be5be12bd4ca19f7
AleksandrSterne <laki1593@outlook.com>
2023-04-06 19:51:37 +0200

    Working on theme settings, taking a break

--- src/App.js
+++ src/App.js
@@ -54,12 +54,15 @@ export const App = () => {
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? 'md:ml-72' : 'flex-2'
-            }`}
+            }
+            `}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
             <div>
+              <ThemeSettings />
+
               <Routes>
                 {/* Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
